2c86888620c7c0a4dba8ff7773bac0129353cb8b
Andy Maskiell <maskiella@gmail.com>
2017-11-05 22:24:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,6 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combo|
     board[win_combo[0]] == board[win_combo[1]] && board[win_combo[2]] == board[win_combo[1]]
-    position_taken?(board, win_combo[0])
     # (board[win_combo[0]] == board[win_combo[1]] == board[win_combo[2]] == "X") || (board[win_combo[0]] == board[win_combo[1]] == board[win_combo[2]] == "O")
   end
 end
